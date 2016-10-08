d84f7547e09b2dfc937a685cd83c615a1d30c394
Sam <sayid.malekdoost@gmail.com>
2016-10-08 09:14:17 +0200

    last changes

--- Assets/Scripts/DEMO/LanguageManager/ITutorialEvents.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface ITutorialEvents{
-	void UserDidEnterTutorialSequence (SubtitleMessage.TutorialMessagesStruct[] trigger);
-	void UserDidExitTutorialSequence (SubtitleMessage.TutorialMessagesStruct[] trigger, bool removeTutorialOnExit);
-}
